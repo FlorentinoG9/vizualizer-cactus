5efe5dbdc9cedc025c7df8cd764d27a237519dc2
FlorentinoG9 <f.valle.garcia@gmail.com>
2021-12-26 13:23:02 -0600

    create pallet component

---
+++ components/Pallet.jsx
@@ -0,0 +1,13 @@
+import { useContext } from 'react';
+import { MaterialContext } from '../contexts/MaterialContext';
+
+export default function Pallet() {
+  const { pallet } = useContext(MaterialContext);
+
+  return (
+    <div>
+      
+      
+    </div>
+  )
+}
